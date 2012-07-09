9a4bfaa5b6e6cf0bccab30a0dff498fbed1593b2
5005688 <syukatu251@gmail.com>
2012-07-09 14:05:08 +0900

    initialize ClipPage.

---
+++ js/background/bg.js
@@ -0,0 +1,20 @@
+﻿var bg = {
+    captureWholePage: function(callback) {
+        var self = this;
+
+        bgContentScripts.page.init(function() {
+            bgContentScripts.canvas.init(bgContentScripts.page.getSizeDocument());
+            bgContentScripts.page.captureWholePage(function() {
+                bgPlugin.save();
+            });
+        });
+    }
+};
+
+var bgPlugin = {
+    save: function() {
+        var strPngBase64 = bgContentScripts.canvas.getSrcImage();
+        var plugin = document.getElementById('idPlugin');
+        plugin.launchWpPreview(strPngBase64, 'test', 'test.html');
+    }
+};
